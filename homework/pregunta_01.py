eb8779415f8560c87f9a5a9bb5bd9642f652305c
yacardonama <yacardonama@unal.edu.co>
2024-12-17 11:43:03 -0500

    Dic_17

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -13,3 +13,12 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    #sexo = 2 
+    #tipo de emprendimiento = 4 
+    #idea de negocio = 75 
+    #barrio = 225
+    #estrato= 4
+    #comuna = 21
+    #fecha = 795
+    #monto = 277
+    #linea crédito = 9
