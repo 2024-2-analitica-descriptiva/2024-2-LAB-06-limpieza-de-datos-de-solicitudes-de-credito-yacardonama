7587779b4c5c96e7d72de12f39fe387499020a3a
yacardonama <yacardonama@unal.edu.co>
2024-12-20 16:36:10 -0500

    Dic_20
    Limpiar comentarios, eliminar notebook y dejar solo módulo

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,7 +1,8 @@
 """
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
-
+import pandas as pd
+import os
 
 def pregunta_01():
     """
@@ -22,3 +23,61 @@ def pregunta_01():
     #fecha = 795
     #monto = 277
     #linea crédito = 9
+
+    ruta= "./files/input/solicitudes_de_credito.csv"
+    df = pd.read_csv(ruta, sep=';')
+
+    df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
+
+    df['sexo'] = df['sexo'].str.lower()
+
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
+
+    df['idea_negocio'] = df['idea_negocio'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].replace('-', ' ', regex=True)
+    df['idea_negocio'] = df['idea_negocio'].replace('_', ' ', regex=True)
+    df['idea_negocio'] = df['idea_negocio'].str.strip()
+
+    df['barrio'] = df['barrio'].str.lower()
+    df['barrio'] = df['barrio'].str.replace(r'\.', ' ', regex=True)
+    df['barrio'] = df['barrio'].replace('_', ' ', regex=True)
+    df['barrio'] = df['barrio'].replace('-', ' ', regex=True)
+
+    df['estrato'] = df['estrato'].astype(str)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(str)
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('$', '').str.replace(',', '')
+    df['monto_del_credito'] = df['monto_del_credito'].astype(float).astype(int)
+
+    df['línea_credito'] = df['línea_credito'].str.lower()
+    df['línea_credito'] = df['línea_credito'].str.strip()
+    df['línea_credito'] = df['línea_credito'].str.replace(r'\.', ' ', regex=True)
+    df['línea_credito'] = df['línea_credito'].replace('-', ' ', regex=True)
+    df['línea_credito'] = df['línea_credito'].replace('_', ' ', regex=True)
+    df['línea_credito'] = df['línea_credito'].replace('  ', ' ', regex=True)
+
+    # Función para convertir a formato dd/mm/yyyy
+    def convertir_fecha(fecha):
+        for fmt in ['%d/%m/%Y', '%Y/%m/%d', '%d-%m-%Y', '%Y%m%d']:
+            try:
+                return pd.to_datetime(fecha, format=fmt).strftime('%d/%m/%Y')
+            except ValueError:
+                pass
+        return None  # Si no coincide con ningún formato
+
+    # Aplicar la función a la columna 'fecha'
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(convertir_fecha)
+
+    df = df.drop(['Unnamed: 0'], axis=1)
+    df.drop_duplicates(inplace=True)
+
+    ruta_archivo = "./files/output/solicitudes_de_credito.csv"
+
+    directorio = os.path.dirname(ruta_archivo)
+    if directorio: 
+        os.makedirs(directorio, exist_ok=True)
+
+    df.to_csv(ruta_archivo, sep=";", index=False, encoding='utf-8')
+
+pregunta_01()
